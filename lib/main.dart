0896dd0bfd2055ede2999e495ca0292b933f2eeb
Favour Okunowo <43044740+NowoCodes@users.noreply.github.com>
2021-03-24 18:07:11 +0100

    Update main.dart
    Showing the stressful way of passing data across 3 levels (statless widgets)

--- lib/main.dart
+++ lib/main.dart
@@ -5,40 +5,54 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final String data = 'Top Secret Data';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Container(),
+          title: Text(data),
         ),
-        body: Level1(),
+        body: Level1(data),
       ),
     );
   }
 }
 
 class Level1 extends StatelessWidget {
+  final String data;
+  Level1(this.data);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level2(),
+      child: Level2(data),
     );
   }
 }
 
 class Level2 extends StatelessWidget {
+  final String data;
+  Level2(this.data);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Level3(),
+    return Column(
+      children: [
+        Container(),
+        Level3(data),
+      ],
     );
   }
 }
 
 class Level3 extends StatelessWidget {
+  final String data;
+  Level3(this.data);
+
   @override
   Widget build(BuildContext context) {
-    return Text('Needs Data');
+    return Text(data);
   }
 }
